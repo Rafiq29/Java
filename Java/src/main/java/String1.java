1dffe740d793b267a2bde2f185705c51f0afb00c
Rafiq29 <r29qifar@gmail.com>
2019-10-16 17:02:29 +0400

    string generation

---
+++ Java/src/main/java/String1.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class String1 {
+    public static void main(String[] args) {
+
+        Random random= new Random();
+        for (int i = 0; i <5 ; i++) {
+            char number= (char)(random.nextInt('Z'-'A'+1)+'A');
+            String generate= " ";
+            generate+=number;
+            System.out.print(generate);
+        }
+    }
+}
