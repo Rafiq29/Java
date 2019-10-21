c8ca26a8dae044fb12afeced32574033ab1ddffd
pointdisarr <ibrahimov1997@gmail.com>
2019-10-21 16:39:59 +0400

    --compare

---
+++ Java/src/main/java/ComparingString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ComparingString {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String st1= sc.nextLine();
+        String st2= sc.nextLine();
+        System.out.println(st1+st2);
+
+
+        }
+    }
+
