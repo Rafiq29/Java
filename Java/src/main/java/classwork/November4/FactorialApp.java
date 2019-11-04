106291e208f480abff560f07171d676dcd23ab08
Cavoli <cavoli309@gmail.com>
2019-11-04 18:29:59 +0400

    -factorial

---
+++ Java/src/main/java/classwork/November4/FactorialApp.java
@@ -0,0 +1,14 @@
+package classwork.November4;
+
+public class FactorialApp {
+    public static void main(String[] args) {
+        FactorialApp app= new FactorialApp();
+        int f5 = app.fact(15);
+        System.out.println(f5);
+    }
+
+    private int fact(int value) {
+        if (value==1) return 1;
+        return value*fact(value-1);
+    }
+}
