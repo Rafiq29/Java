423bffb4420237158cec3a975db27487cf87d4dc
Cavoli <cavoli309@gmail.com>
2019-11-06 16:59:48 +0400

    -warmup

---
+++ Java/src/main/java/classwork/November6/Solution.java
@@ -0,0 +1,28 @@
+package classwork.November6;
+
+public class Solution {
+    public static void main(String[] args) {
+        String S = "RLRLRLRRLRLRLL";
+        System.out.println(solution(S));
+    }
+
+    public static int solution(String s) {
+        int counter = 0;
+        int max_counter=0;
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'R':
+                    counter++;
+                    break;
+                case 'L':
+                    counter--;
+                    break;
+                default:
+                   throw new IllegalArgumentException("Wrong character");
+            }
+            if (counter==0) max_counter++;
+        }
+        if (counter != 0) throw new  IllegalArgumentException("Doesn't match");
+        return max_counter;
+    }
+}
---
+++ Java/src/main/java/classwork/November6/Warmup.java
@@ -0,0 +1,7 @@
+package classwork.November6;
+
+public class Warmup {
+    public static void main(String[] args) {
+        new Solution();
+    }
+}
