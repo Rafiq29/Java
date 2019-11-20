0efa9212363759dbbd37fe92c353324200d338eb
wewe <weewe>
2019-11-20 16:53:46 +0400

    -warmup+javaweb/task

---
+++ Java/src/main/java/classwork/November20/ByeServlet.java
@@ -0,0 +1,17 @@
+package classwork.November20;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ByeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.printf("Bye, %s", req.getPathInfo().substring(1));
+        writer.close();
+    }
+}
