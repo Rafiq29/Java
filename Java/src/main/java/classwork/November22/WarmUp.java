c9b765429a8173540104bf1d5dc1c1a416ce095e
wewe <weewe>
2019-11-22 18:30:31 +0400

    -sql

---
+++ Java/src/main/java/classwork/November22/WarmUp.java
@@ -0,0 +1,24 @@
+//package classwork.November22;
+////
+////import java.util.ArrayList;
+////import java.util.Scanner;
+////
+////public class WarmUp {
+////    public static void main(String[] args) {
+////        Scanner sc = new Scanner(System.in);
+////        int n = sc.nextInt();
+////        System.out.println(odd_el(n));
+////    }
+////
+////    private static int odd_el(int n) {
+////        int i = 3;
+////        int counter=0;
+////        while (i % 3 == 0) {
+////            if (i % 2 != 0){ int_arr.add(i);}
+////            counter++;
+////        }
+////       if (int_arr.indexOf(n) % 2 == 0) return int_arr.indexOf(n);
+////       else return odd_el(n+1);
+////    }
+////}
+//TODO complete
