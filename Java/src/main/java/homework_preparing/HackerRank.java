178c7a8fd18c1a707dcb1f0696cad6f80fdc2ba9
Rafiq29 <r29qifar@gmail.com>
2019-11-27 00:45:07 +0400

    -hacker_rank

---
+++ Java/src/main/java/homework_preparing/HackerRank.java
@@ -0,0 +1,10 @@
+package homework_preparing;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class HackerRank {
+   // Will test code
+}
