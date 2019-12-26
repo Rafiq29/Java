69454a56d8d927f8af1a99ddd1dc0c69597ea2f9
RafiqB <r29qifar@gmail.com>
2019-12-26 23:50:15 +0400

    -hacker_rank task 8

--- Java/src/main/java/homework_preparing/HackerRank.java
+++ Java/src/main/java/homework_preparing/HackerRank.java
@@ -1,10 +1,30 @@
 package homework_preparing;
-import java.io.*;
-import java.math.*;
-import java.text.*;
-import java.util.*;
-import java.util.regex.*;
 
-public class HackerRank {
-   // Will test code
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        Map <String,Integer> pbook = new HashMap<>();
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            pbook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if (pbook.containsKey(s)) {
+                System.out.println(s+"="+pbook.get(s));
+            }
+            else System.out.println("Not found");
+        }
+        in.close();
+    }
 }
+
+
